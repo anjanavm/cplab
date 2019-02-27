52e0b59fcab92a9f0b0ecf084251d0e3a7644849
anjanavm <47378774+anjanavm@users.noreply.github.com>
2019-02-27 12:25:01 +0530

    Add files via upload

---
+++ time.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+int time,i,j;
+printf("12 midnight\n");
+for(i=1;i<12;i++)
+
+ printf("%d AM\n",i);
+   printf("12 noon\n");
+   for(j=1;j<=11;j++)
+        printf("%d PM\n",j);
+ 
+}
